7545410208c0616020a5900e78f4afa0eef7059d
Manuel <info@tagirijus.de>
2025-04-11 06:47:31 +0200

    added template config file

---
+++ todomanager/model/config.dist.py
@@ -0,0 +1,14 @@
+'''
+The config for the todomanager website.
+
+Copy this file to "config.py" and fill in the needed variables.
+'''
+
+
+# NEXTCLOUD settings
+
+NC_URI = ''
+NC_USER = ''
+NC_PASSWORD = ''
+NC_CALENDAR = ''
+NC_TAGS = []
